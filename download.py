3e48270fdbac598ad403a999629290325cb173ef
Wubaboo <evan4217@gmail.com>
2021-12-25 00:56:47 -0500

    convert audio to mp3 files

--- download.py
+++ download.py
@@ -1,4 +1,5 @@
 import pafy
+from pydub import AudioSegment
 
 '''
  Given a URL, download the video, or audo, or both
@@ -11,13 +12,16 @@ class Video():
     def __init__(self, url):
         self.url = url
         self.video = pafy.new(url)
+    
+    def convert(self, fname, dest):
+        downloaded = AudioSegment.from_file(fname, fname.split('.')[-1])
+        downloaded.export(dest, format='mp3')
         
     ## Download the youtube video from self.url saving it with the
     ## given filename
     def download(self, filename = '', audio = True, video = True):
         if audio and video:
-            self.video.getbest().download(filename)
+            self.video.getbest().download()
         elif audio:
-            self.video.getbestaudio(preftype = 'mp3').download(filename)
-        elif video:
-            self.video.getbestvideo(preftype = 'mp4').download(filename)
+            aud_fname = self.video.getbestaudio().download()
+            self.convert(aud_fname, f'downloads/{filename}.mp3')
